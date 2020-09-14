5271f88820e64ac6faaa61b4f8b2aa4cc2c9378f
yuliyagordy <yuliyagordy@gmail.com>
2020-09-14 15:16:35 -0400

    query by symbol and create new record functions implemented

--- src/main/java/com/ygordy/stocks/controller/DowJonesStockController.java
+++ src/main/java/com/ygordy/stocks/controller/DowJonesStockController.java
@@ -1,21 +1,23 @@
 package com.ygordy.stocks.controller;
 
 import com.ygordy.stocks.service.DowJonesStockService;
+import com.ygordy.stocks.service.dto.DowJonesStock;
 import com.ygordy.stocks.util.HttpHeadersBuilder;
 import com.ygordy.stocks.util.StocksAppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class DowJonesStockController {
+    private static final Logger log = LoggerFactory.getLogger(DowJonesStockController.class);
 
     @Autowired
     DowJonesStockService dowJonesStockService;
@@ -23,6 +25,7 @@ public class DowJonesStockController {
     @PostMapping("/djstock/bulkupload")
     public ResponseEntity bulkStockUpload(@RequestParam("file") MultipartFile multipartFile,
                                           @RequestParam("setupByUserId") String userId) {
+        log.debug("bulkStockUpload() setupByUserId=["+ userId + "]");
         try {
             dowJonesStockService.saveBulkStocks(userId, multipartFile);
             String message = "Uploaded the file successfully: " + multipartFile.getOriginalFilename();
@@ -36,5 +39,36 @@ public class DowJonesStockController {
                             "Failed to save bulk records of DowJonesStock"))
                     .body(message);
         }
+    }
+
+    @GetMapping("/djstock/list/{symbolId}")
+    public ResponseEntity<List<DowJonesStock>> getStockList(@RequestParam("setupByUserId") String userId,
+                                                            @PathVariable String symbolId) throws Exception {
+        log.debug("getStockList() setupByUserId=["+ userId + "]");
+        List<DowJonesStock> dowJonesStockList = dowJonesStockService.findStockListBySymbol(symbolId);
+        if (dowJonesStockList == null || dowJonesStockList.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT)
+                    .headers(HttpHeadersBuilder.createWarningAlert(StocksAppConstants.DJ_ENTITY_NAME, "null", "List empty"))
+                    .body(null);
+        }
+        return ResponseEntity.created(new URI("/api/djstock/list"))
+                .headers(HttpHeadersBuilder.createGetStockListAlert(StocksAppConstants.DJ_ENTITY_NAME))
+                .body(dowJonesStockList);
+    }
+
+    @PostMapping("/djstock/create")
+    public ResponseEntity<DowJonesStock> createStock(@RequestParam("setupByUserId") String userId,
+                                                     @RequestBody DowJonesStock dowJonesStock) throws Exception {
+        log.debug("createStock() setupByUserId=["+ userId + "]");
+        DowJonesStock result = dowJonesStockService.addDowJonesStock(userId, dowJonesStock);
+        if (result == null) {
+            return ResponseEntity.badRequest()
+                    .headers(HttpHeadersBuilder.createFailureAlert(StocksAppConstants.DJ_ENTITY_NAME, "failed",
+                            "Failed to create new DowJonesStock"))
+                    .body(null);
+        }
+        return ResponseEntity.created(new URI("/api/stock/create" + result.getId()))
+                .headers(HttpHeadersBuilder.createEntityCreatedAlert(StocksAppConstants.DJ_ENTITY_NAME, result.getId().toString()))
+                .body(result);
     }
 }
--- src/main/java/com/ygordy/stocks/service/DowJonesStockService.java
+++ src/main/java/com/ygordy/stocks/service/DowJonesStockService.java
@@ -2,6 +2,7 @@ package com.ygordy.stocks.service;
 
 import com.ygordy.stocks.model.DowJonesStockEntity;
 import com.ygordy.stocks.repository.DowJonesStockRepository;
+import com.ygordy.stocks.service.dto.DowJonesStock;
 import com.ygordy.stocks.service.dtoconverter.DowJonesConverterToDTO;
 import com.ygordy.stocks.service.dtoconverter.DowJonesConverterToEntity;
 import com.ygordy.stocks.util.CSVFileHelper;
@@ -9,10 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class DowJonesStockService {
+    private static final Logger log = LoggerFactory.getLogger(DowJonesStockService.class);
 
     @Autowired
     private final DowJonesStockRepository dowJonesStockRepository;
@@ -41,5 +47,31 @@ public class DowJonesStockService {
         } catch (Exception e) {
             throw new RuntimeException("fail to store csv data: " + e.getMessage());
         }
+    }
+
+    public List<DowJonesStock> findStockListBySymbol(String symbolId) {
+        List<DowJonesStockEntity> stockEntityList = dowJonesStockRepository.findAllByStockSymbol(symbolId);
+        List<DowJonesStock> dowJonesStockList = null;
+        if (stockEntityList != null) {
+            dowJonesStockList = new ArrayList();
+            for (DowJonesStockEntity stockEntity : stockEntityList) {
+                dowJonesStockList.add(dowJonesConverterToDTO.apply(stockEntity));
+            }
+        }
+        return dowJonesStockList;
+    }
+
+
+    public DowJonesStock addDowJonesStock(String userIdStr, DowJonesStock dowJonesStock) {
+        DowJonesStockEntity dowJonesStockEntity = dowJonesConverterToEntity.apply(dowJonesStock);
+        LocalDateTime now = LocalDateTime.now();
+        dowJonesStockEntity.setModifiedDate(now);
+        Long userId = Long.parseLong(userIdStr);
+        dowJonesStockEntity.setSetupByUserId(userId);
+        DowJonesStockEntity newDowJonesStockEntity = dowJonesStockRepository.save(dowJonesStockEntity);
+        if (newDowJonesStockEntity != null) {
+            return dowJonesConverterToDTO.apply(newDowJonesStockEntity);
+        }
+        return null;
     }
 }
